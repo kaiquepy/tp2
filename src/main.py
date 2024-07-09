50be7904c375e326856e6b932eb72423580ceb1b
Kaique Vieira <kvieiramiranda@gmail.com>
2024-07-09 10:11:01 -0300

    adicionando tratamento de erros

--- src/main.py
+++ src/main.py
@@ -3,11 +3,12 @@ Módulo principal do programa.
 Contém a função main_menu que exibe o menu principal e lida com as escolhas do usuário.
 """
 
+from typing import Dict
 from huffman import huffman_encode, huffman_decode
 from utils import save_to_file, read_from_file, clear_screen
 
 
-def main_menu():
+def main_menu() -> None:
     """
     Exibe o menu principal e lida com as escolhas do usuário.
     """
@@ -18,29 +19,58 @@ def main_menu():
         print("2. Decodificar arquivo")
         print("3. Sair")
 
-        choice = input("Escolha uma opção: ")
+        choice: str = input("Escolha uma opção: ")
 
         if choice == '1':
             clear_screen()
             print(f"{'='*10} Codificar arquivo {'='*10}")
-            input_file = input("Digite o nome do arquivo de entrada (ex: entrada.txt): ")
-            output_file = input("Digite o nome do arquivo de saída (ex: saida.huf): ")
-            input_text = read_from_file(input_file)
-            encoded_text, huffman_codes = huffman_encode(input_text)
-            save_to_file(output_file, encoded_text)
-            print(f"Arquivo codificado salvo em {output_file}")
-            input("Pressione Enter para continuar...")
+            input_file: str = input("Digite o nome do arquivo de entrada (ex: entrada.txt): ")
+            output_file: str = input("Digite o nome do arquivo de saída (ex: saida.huf): ")
 
+            try:
+                input_text: bytes = read_from_file(input_file)
+            except FileNotFoundError:
+                print(f"Erro: O arquivo '{input_file}' não foi encontrado.")
+                input("Pressione Enter para continuar...")
+                continue
+            except Exception as e:
+                print(f"Erro ao ler o arquivo '{input_file}': {e}")
+                input("Pressione Enter para continuar...")
+                continue
+
+            try:
+                encoded_text: bytes
+                huffman_codes: Dict[bytes, str]
+                encoded_text, huffman_codes = huffman_encode(input_text)
+                save_to_file(output_file, encoded_text)
+                print(f"Arquivo codificado salvo em {output_file}")
+            except Exception as e:
+                print(f"Erro ao codificar o arquivo '{input_file}': {e}")
+            input("Pressione Enter para continuar...")
 
         elif choice == '2':
             clear_screen()
             print(f"{'='*10} Decodificar arquivo {'='*10}")
-            input_file = input("Digite o nome do arquivo codificado (ex: saida.huf): ")
-            output_file = input("Digite o nome do arquivo de saída para o texto decodificado: ")
-            encoded_text = read_from_file(input_file)
-            decoded_text = huffman_decode(encoded_text, huffman_codes)
-            save_to_file(output_file, decoded_text)
-            print(f"Arquivo decodificado salvo em {output_file}")
+            input_file: str = input("Digite o nome do arquivo codificado (ex: saida.huf): ")
+            output_file: str = input("Digite o nome do arquivo de saída para o texto decodificado: ")
+
+            try:
+                encoded_text: bytes = read_from_file(input_file)
+            except FileNotFoundError:
+                print(f"Erro: O arquivo '{input_file}' não foi encontrado.")
+                input("Pressione Enter para continuar...")
+                continue
+            except Exception as e:
+                print(f"Erro ao ler o arquivo '{input_file}': {e}")
+                input("Pressione Enter para continuar...")
+                continue
+
+            try:
+                decoded_text: bytes = huffman_decode(encoded_text, huffman_codes)
+                save_to_file(output_file, decoded_text)
+                print(f"Arquivo decodificado salvo em {output_file}")
+            except Exception as e:
+                print(f"Erro ao decodificar o arquivo '{input_file}': {e}")
             input("Pressione Enter para continuar...")
             clear_screen()
 
@@ -50,6 +80,7 @@ def main_menu():
 
         else:
             print("Opção inválida. Por favor, escolha novamente.")
+
 
 if __name__ == "__main__":
     try:
