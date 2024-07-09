adfb12312aa570358cfdeb2295775a98e4c7be60
Kaique Vieira <kvieiramiranda@gmail.com>
2024-07-09 00:27:34 -0300

    melhorando o menu da aplicação

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from huffman import huffman_encode, huffman_decode, save_to_file, read_from_file
+from huffman import huffman_encode, huffman_decode
+from utils import save_to_file, read_from_file, clear_screen
 
 
 def main_menu():
     while True:
-        print("\nMenu:")
+        clear_screen()
+        print(f"{'='*10} Menu {'='*10}")
         print("1. Codificar arquivo")
         print("2. Decodificar arquivo")
         print("3. Sair")
@@ -11,20 +13,28 @@ def main_menu():
         choice = input("Escolha uma opção: ")
 
         if choice == '1':
+            clear_screen()
+            print(f"{'='*10} Codificar arquivo {'='*10}")
             input_file = input("Digite o nome do arquivo de entrada (ex: entrada.txt): ")
             output_file = input("Digite o nome do arquivo de saída (ex: saida.huf): ")
             input_text = read_from_file(input_file)
             encoded_text, huffman_codes = huffman_encode(input_text)
             save_to_file(output_file, encoded_text)
-            print(f"Texto codificado salvo em {output_file}")
+            print(f"Arquivo codificado salvo em {output_file}")
+            input("Pressione Enter para continuar...")
+
 
         elif choice == '2':
+            clear_screen()
+            print(f"{'='*10} Decodificar arquivo {'='*10}")
             input_file = input("Digite o nome do arquivo codificado (ex: saida.huf): ")
             output_file = input("Digite o nome do arquivo de saída para o texto decodificado: ")
             encoded_text = read_from_file(input_file)
             decoded_text = huffman_decode(encoded_text, huffman_codes)
             save_to_file(output_file, decoded_text)
-            print(f"Texto decodificado salvo em {output_file}")
+            print(f"Arquivo decodificado salvo em {output_file}")
+            input("Pressione Enter para continuar...")
+            clear_screen()
 
         elif choice == '3':
             print("Encerrando o programa...")
