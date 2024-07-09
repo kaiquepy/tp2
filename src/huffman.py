af17af6a9155730055425f82752c64f2c7ce9d41
Kaique Vieira <kvieiramiranda@gmail.com>
2024-07-08 23:57:45 -0300

    alterando para permitir compactar arquivos binários

--- src/huffman.py
+++ src/huffman.py
@@ -1,4 +1,5 @@
 import heapq
+import os
 
 class HuffmanNode:
     def __init__(self, char, freq):
@@ -11,16 +12,16 @@ class HuffmanNode:
     def __lt__(self, other):
         return self.freq < other.freq
 
-def build_frequency_dict(text):
+def build_frequency_dict(data):
     freq = {}
-    for char in text:
-        if char not in freq:
-            freq[char] = 0
-        freq[char] += 1
+    for byte in data:
+        if byte not in freq:
+            freq[byte] = 0
+        freq[byte] += 1
     return freq
 
 def build_huffman_tree(freq_dict):
-    heap = [HuffmanNode(char, freq) for char, freq in freq_dict.items()]
+    heap = [HuffmanNode(byte, freq) for byte, freq in freq_dict.items()]
     heapq.heapify(heap)
 
     while len(heap) > 1:
@@ -33,40 +34,40 @@ def build_huffman_tree(freq_dict):
 
     return heap[0]
 
-def build_codes_dict(node, prefix="", codes=None):
+def build_codes_dict(node, prefix=b"", codes=None):
     if codes is None:
         codes = {}
     if node is not None:
         if node.char is not None:
             codes[node.char] = prefix
-        build_codes_dict(node.left, prefix + "0", codes)
-        build_codes_dict(node.right, prefix + "1", codes)
+        build_codes_dict(node.left, prefix + b"0", codes)
+        build_codes_dict(node.right, prefix + b"1", codes)
     return codes
 
-def huffman_encode(text):
-    freq_dict = build_frequency_dict(text)
+def huffman_encode(data):
+    freq_dict = build_frequency_dict(data)
     huffman_tree = build_huffman_tree(freq_dict)
     huffman_codes = build_codes_dict(huffman_tree)
-    encoded_text = ''.join(huffman_codes[char] for char in text)
-    return encoded_text, huffman_codes
+    encoded_data = b"".join([huffman_codes[byte] for byte in data])
+    return encoded_data, huffman_codes
 
-def huffman_decode(encoded_text, huffman_codes):
+def huffman_decode(encoded_data, huffman_codes):
     reverse_codes = {v: k for k, v in huffman_codes.items()}
-    current_code = ""
-    decoded_text = []
+    current_code = b""
+    decoded_data = bytearray()
 
-    for bit in encoded_text:
-        current_code += bit
+    for bit in encoded_data:
+        current_code += bit.to_bytes(1, byteorder="big")
         if current_code in reverse_codes:
-            decoded_text.append(reverse_codes[current_code])
-            current_code = ""
+            decoded_data.append(reverse_codes[current_code])
+            current_code = b""
 
-    return ''.join(decoded_text)
+    return bytes(decoded_data)
 
 def save_to_file(filename, data):
-    with open(filename, 'w') as file:
+    with open(filename, "wb") as file:
         file.write(data)
 
 def read_from_file(filename):
-    with open(filename, 'r') as file:
+    with open(filename, "rb") as file:
         return file.read()
